0535cb8d8a8400ced75b6271c6fce0e0f5b17c27
Sanghyeon <zerojsh00@nate.com>
2018-07-15 22:26:16 +0900

    Revert "including text_test.txt"
    This reverts commit f3d2cf48ccac0f3d670032851ea9dda9556b4f4f.

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 # For command line git tutorial
 # First : GitHub modification
 # Second : Local repository modification
 
 print("Conflict Resolved! ")
-=======
-print("Hello World")
->>>>>>> parent of 94b01bd... hello.py modified on Local Repository
