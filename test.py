f3d2cf48ccac0f3d670032851ea9dda9556b4f4f
Sanghyeon <zerojsh00@nate.com>
2018-07-15 22:25:42 +0900

    including text_test.txt

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 # For command line git tutorial
 # First : GitHub modification
 # Second : Local repository modification
 
 print("Conflict Resolved! ")
+=======
+print("Hello World")
+>>>>>>> parent of 94b01bd... hello.py modified on Local Repository
